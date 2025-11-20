b510fe22d71265efbaf6e1b7a8d25be0ccf72cc1
Vishal Singh <vish.singh3020@gmail.com>
2025-11-20 21:17:55 +0530

    Nextjs frontend Clone

---
+++ app/showcase/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-[#000000] h-screen pt-20 flex-col items-center justify-center text-center space-y-6">Showcase</div>
+  )
+}
+
+export default page
