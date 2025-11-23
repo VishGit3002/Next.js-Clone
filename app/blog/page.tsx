405b02a12fc903ba3297ca743c5927c4199ca4dc
Vishal Singh <vish.singh3020@gmail.com>
2025-11-23 14:01:43 +0530

    fix blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>page</div>
+    <div className="pt-24 min-h-screen px-6 md:px-12 lg:px-24">
+      <h1 className="text-4xl font-bold mb-8">Blog</h1>
+      <p>Welcome to the blog page.</p>
+    </div>
   )
 }
 
